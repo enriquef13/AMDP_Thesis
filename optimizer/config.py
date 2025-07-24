15aa7e3a2e3795c40dd0454cc3f4b726ecb1bc8a
enriquef13 <enriquef@mit.edu>
2025-07-24 14:23:01 -0400

    Final floor optimizer

--- optimizer/config.py
+++ optimizer/config.py
@@ -1,10 +1,11 @@
 import general_data as gd
 
+# Example of HXV3 basin (22 ft x 14 ft x 26 in)
 submodule_type = 'Water Collection Welded'      # Type of submodule
 material = gd.SST                               # Material type (gd.SST or gd.GLV)
 x_in = 315                                      # Longer dimension in inches
 y_in = 160                                      # Shorter dimension in inches
-z_in = 26                                       # Basin height in inches
+z_in = 30                                       # Basin height in inches
 water_height_in = 21                            # Water height in inches
 wind_zone = gd.WIND_NTC                         # Wind zone classification (gd.WIND_NTC, gd.WIND_TC, or gd.WIND_TCM)
 top_load = 11540.0584                           # Uniform load on top edges (lbf)
